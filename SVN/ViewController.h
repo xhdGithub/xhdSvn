d93540c4dbe311031b7338a818ce5c18a0b1b9a8
echo231 <echo231@bogon>
2016-01-09 19:44:01 +0800

    Initial Commit

---
+++ SVN/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SVN
+//
+//  Created by echo231 on 16/1/9.
+//  Copyright © 2016年 echo231. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SVNTests/SVNTests.m
@@ -0,0 +1,39 @@
+//
+//  SVNTests.m
+//  SVNTests
+//
+//  Created by echo231 on 16/1/9.
+//  Copyright © 2016年 echo231. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SVNTests : XCTestCase
+
+@end
+
+@implementation SVNTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
